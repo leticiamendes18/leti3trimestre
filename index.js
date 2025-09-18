82e5dbec8a0e583ec872404be1ba57f78e90724f
leticiamendes18 <145469245+leticiamendes18@users.noreply.github.com>
2025-09-18 18:26:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>!";
+
+console.log( "Oi isso é um um console.log");document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>!";
 console.log( "Oi isso é um um console.log");
+
+let a,b,c; 
+ a= 5;
+ b= 6;
+ c= 7;
+
+ let pessoa = "Dimitri";
+ let pesso = "Dimitri";
+
+    function minhafuncao (){
+        document.write(5 + 5);
+        document.write(5 + 5);
+        document.write(5 + 5);
+        document.write(5 + 5);
+    }
