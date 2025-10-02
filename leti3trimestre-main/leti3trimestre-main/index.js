e1516072bb9eccec85f4445e76dc3d5d7fe62469
leticiamendes18 <145469245+leticiamendes18@users.noreply.github.com>
2025-10-02 18:14:57 -0300

    Add files via upload

---
+++ leti3trimestre-main/leti3trimestre-main/index.js
@@ -0,0 +1,17 @@
+document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>!";
+console.log( "Oi isso é um um console.log");
+
+let a,b,c; 
+ a= 5;
+ b= 6;
+ c= 7;
+
+ let pessoa = "Dimitri";
+ let pesso = "Dimitri";
+
+    function minhafuncao (){
+        document.write(5 + 5);
+        document.write(5 + 5);
+        document.write(5 + 5);
+        document.write(5 + 5);
+    }
